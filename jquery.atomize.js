3e57d4065e1c1c82643b3a30edcb006fabc4e4d4
Jeremy Wells <jeremy@boost.co.nz>
2009-07-17 09:38:14 +1200

    initial commit

---
+++ jquery.atomize.js
@@ -0,0 +1,29 @@
+jQuery.fn.atomize = function(text) {
+  this.each(function() {
+    var input = $(this);
+    input.atomText = text;
+    
+    if(input.val() == '') {
+      input.val(text);
+      input.addClass('atom');
+    }
+    
+    $(input.parents('form')).submit(function(event) {
+      if(input.val() == text) input.val('');
+    });
+    
+    input.focus(function(event) {
+      if($(this).val() == text) {
+        $(this).val('');
+        $(this).removeClass('atom');
+      }
+    });
+    
+    input.blur(function(event) {
+      if($(this).val() == '') {
+        $(this).val(text);
+        $(this).addClass('atom');
+      }
+    });
+  });
+}
---
+++ moo.atomizer.js
@@ -0,0 +1,54 @@
+var Atomizer = new Class({
+  initialize: function(input) {
+    this.input = input;
+    this.setup();
+  },
+
+  setup: function() {
+    this.text = this.input.get('rel');
+    this.changed = true;
+
+    if(this.input.value == "" || this.input.value == this.text) {
+      this.changed = false;
+      this.input.set('value', this.text);
+      this.input.addClass('atom');
+    }
+
+    this.input.addEvent('focus', this.focusHandler.bindWithEvent(this));
+    this.input.addEvent('blur', this.blurHandler.bindWithEvent(this));
+
+    this.setupForm();
+  },
+
+  focusHandler: function(event) {
+    if(!this.changed) this.input.set('value', '');
+    this.input.removeClass('atom');
+  },
+
+  blurHandler: function(event) {
+    if(this.input.get('value') != '') this.changed = true;
+    if(!this.changed) this.input.set('value', this.text);
+    if(!this.changed) this.input.addClass('atom');
+  },
+
+  setupForm: function() {
+    this.form = this.input.getParent('form');
+    if(this.form) {
+      this.form.addEvent('submit', this.formSubmitHandler.bindWithEvent(this));
+    }
+  },
+
+  formSubmitHandler: function(event) {
+    if(!this.changed) this.input.value = "";
+  }
+});
+
+Atomizer.atomizeInputs = function() {
+  $$('input').each(function(input) {
+    new Atomizer(input);
+  });
+}
+
+// window.addEvent('domready', function() {
+//   Atomizer.atomizeInputs();
+// });
